4d02e9f02f8bf0dc9060589b6a5a38507b2a6e6b
Alvaro Sanchez <comunidad@atijaguar.com>
2019-02-26 01:39:08 -0500

    Creación de la plantilla principal

--- README.md
+++ README.md
@@ -26,3 +26,7 @@ npm 6.1.0
 3. run `npm start`
 4. Open running code at `http://localhost:8233/`
 
+# Referencias
+https://medium.freecodecamp.org/how-to-combine-webpack-4-and-babel-7-to-create-a-fantastic-react-app-845797e036ff
+
+
--- desktop/webpack.config.js
+++ desktop/webpack.config.js
@@ -1,77 +1,172 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const preCSS = require('precss');
+const autoPrefixer = require('autoprefixer');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/desktop.js'),
-  output: {
-    filename: 'desktop.js',
-    library: 'desktop',
-    libraryTarget: 'amd',
-    path: path.resolve(__dirname, 'build/desktop'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(scss|css|sass)$/,
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // compiles Sass to CSS
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          name:'[name].[ext]',
-          outputPath:'assets' //the icons will be stored in dist/assets folder
-        }
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      __dirname,
-      'node_modules',
-    ],
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: '"format amd";',
-      raw: true,
-    }),
-    new CleanWebpackPlugin(['build/desktop']),
-    new CopyPlugin([
-      {from: path.resolve(__dirname, 'src/desktop.js')}
-    ]),
-  ],
-  devtool: 'source-map',
-  externals: [
-    /^lodash$/,
-    /^single-spa$/,
-    /^react$/,
-    /^react\/lib.*/,
-    /^react-dom$/,
-    /.*react-dom.*/,
-    /^rxjs\/?.*$/,
-  ],
-};
+const isHot = true;//path.basename(require.main.filename) === 'webpack.config.js';
+
+console.log(isHot)
+
+const extractCSS = new MiniCssExtractPlugin({
+	filename: '[name].fonts.css',
+	allChunks: true,
+});
+const extractSCSS = new MiniCssExtractPlugin({
+	filename: '[name].styles.css',
+	allChunks: true,
+});
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+const prodPlugins = [
+	new webpack.DefinePlugin({ // <-- key to reducing React's size
+		'process.env': {
+			NODE_ENV: JSON.stringify('production'),
+		},
+	}),
+	new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
+	new webpack.HotModuleReplacementPlugin(),
+	new webpack.NamedModulesPlugin(),
+	new HardSourceWebpackPlugin(),
+	extractCSS,
+	extractSCSS,
+	new HtmlWebpackPlugin({
+		inject: true,
+		template: './public/index.html',
+	}),
+	new CopyWebpackPlugin(
+		[
+			{ from: './public/img', to: 'img' },
+		],
+		{ copyUnmodified: false },
+	),
+	new CompressionPlugin({
+		asset: '[path].gz[query]',
+		algorithm: 'gzip',
+		test: /\.js$|\.css$|\.html$/,
+		threshold: 10240,
+		minRatio: 0.8,
+	}),
+];
+const devPlugins = [
+	new webpack.HotModuleReplacementPlugin(),
+	new webpack.NamedModulesPlugin(),
+	new HardSourceWebpackPlugin(),
+	extractCSS,
+	extractSCSS,
+	new HtmlWebpackPlugin({
+		inject: true,
+		template: './public/index.html',
+	}),
+	new CopyWebpackPlugin(
+		[
+			{ from: './public/assets/img', to: '/img' },
+			{ from: './public/manifest.json', to: '/' },
+		],
+		{ copyUnmodified: false },
+	),
+	new CompressionPlugin({
+		asset: '[path].gz[query]',
+		algorithm: 'gzip',
+		test: /\.js$|\.css$|\.html$/,
+		threshold: 10240,
+		minRatio: 0.8,
+	}),
+];
+
+module.exports = (env = {}) => ({
+	mode: env.prod ? 'production' : 'development',
+	entry: {
+		index: ['babel-polyfill', `${SRC_DIR}/index.js`],
+		
+	},
+	output: {
+		path: BUILD_DIR,
+		filename: '[name].bundle.js'
+	},
+	// watch: true,
+	devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: BUILD_DIR,
+		//   port: 9001,
+		compress: true,
+		hot: true,
+		open: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',	
+					options: {
+						cacheDirectory: true,
+						presets: ['@babel/preset-react', '@babel/preset-env',],
+						plugins: ['@babel/plugin-syntax-dynamic-import',"@babel/plugin-proposal-class-properties"],
+					},
+				},
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+			},
+			/*{
+				test: /\.(scss)$/,
+				use: [{
+					loader: 'style-loader', // inject CSS to page
+				}, {
+					loader: 'css-loader',
+					options: { alias: { '../img': '../public/img' } },
+				}, {
+					loader: 'postcss-loader', // Run post css actions
+					options: {
+						plugins() { // post css plugins, can be exported to postcss.config.js
+							return [
+								preCSS,
+								autoPrefixer,
+							];
+						},
+					},
+				}, {
+					loader: 'sass-loader', // compiles Sass to CSS
+				}],
+			},*/
+			{
+				test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false, sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ],
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|ico)$/,
+				use: [
+					{
+						// loader: 'url-loader'
+						loader: 'file-loader',
+						options: {
+							name: './img/[name].[hash].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file-loader',
+				options: {
+					name: './fonts/[name].[hash].[ext]',
+				},
+			}],
+	},
+	
+	plugins: env.prod ? prodPlugins : devPlugins,
+});
